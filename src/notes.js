f0968165a995c8d357d0e0e5478a4132b9964ebd
dhruvkumar1805 <dhruvkumar1805@gmail.com>
2023-12-31 10:29:34 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+    {
+        key: 1,
+        title: "Buy Groceries",
+        content:
+            "Purchase vegetables, fruits, milk, and eggs from the supermarket."
+    },
+    {
+        key: 2,
+        title: "Plan Vacation Trip",
+        content:
+            "Research and decide on a destination, book flights, and find accommodation for the upcoming vacation trip."
+    },
+    {
+        key: 3,
+        title: "Call Mom",
+        content:
+            "Give Mom a call to check in and see how she's doing."
+    },
+    {
+        key: 4,
+        title: "Fix Bug in Homepage",
+        content:
+            "Investigate and resolve the issue causing the homepage to display incorrectly."
+    }
+];
+
+export default notes;
